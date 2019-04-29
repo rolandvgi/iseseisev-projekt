1b4366e8a0c4f26679aa8d5eeb3af8c20a0a8859
stevsal <stevsal@users.noreply.github.com>
2019-04-29 12:31:15 +0300

    Modal finished?

--- astra.php
+++ astra.php
@@ -12,21 +12,21 @@
 </head>
 <body>
     <h3>0 korrus</h3>
-    <img id="floor0" src="pildid/AstraSilva/Astra-Silva0.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <img class="ASpics" src="pildid/AstraSilva/Astra-Silva0.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
     <h3>1 korrus</h3>
-    <img src="pildid/AstraSilva/Astra-Silva1.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <img class="ASpics" src="pildid/AstraSilva/Astra-Silva1.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
     <h3>2 korrus</h3>
-    <img src="pildid/AstraSilva/Astra-Silva2.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <img class="ASpics" src="pildid/AstraSilva/Astra-Silva2.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
     <h3>3 korrus</h3>
-    <img src="pildid/AstraSilva/Astra-Silva3.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <img class="ASpics" src="pildid/AstraSilva/Astra-Silva3.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
     <h3>4 korrus</h3>
-    <img src="pildid/AstraSilva/Astra-Silva4.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <img class="ASpics" src="pildid/AstraSilva/Astra-Silva4.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
     <h3>5 korrus</h3>
-    <img src="pildid/AstraSilva/Astra-Silva5.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <img class="ASpics" src="pildid/AstraSilva/Astra-Silva5.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
     <!-- The Modal -->
     <div id="myModal" class="modal">
     <!-- The Close Button -->
-    <span class="close">&times;</span>
+    <span class="close" onclick="document,getElementById('myModal').style.display='none'">&times;</span>
     <!-- Modal Content (The Image) -->
     <img class="modal-content" id="img01">
 
@@ -34,5 +34,6 @@
     <div id="caption"></div>
   </div>
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="modal.js"></script>
 </html>
--- modal.js
+++ modal.js
@@ -2,22 +2,24 @@
 
 
 
-window.onload = openmodal();
+/*window.onload = openmodal();
 
 function openmodal() {
-  console.log("funktsioon starts");
+  console.log("funktsioon starts");*/
   // Get the modal
   var modal = document.getElementById('myModal');
 
   // Get the image and insert it inside the modal - use its "alt" text as a caption
-  var img = document.getElementById('floor0');
-  var modalImg = document.getElementById("img01");
+  //code snatched from
+  var img = $('.ASpics');
+  var modalImg = $("#img01");
   var captionText = document.getElementById("caption");
-  img.onclick = function(){
+  $('.ASpics').click(function(){
     modal.style.display = "block";
-    modalImg.src = this.src;
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
     captionText.innerHTML = this.alt;
-  };
+  });
 
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
@@ -26,4 +28,4 @@ function openmodal() {
   span.onclick = function() {
     modal.style.display = "none";
   };
-}
+//}
