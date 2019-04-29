109c453a776d9c4fde9b84a9a5776c420dba725e
stevsal <stevsal@users.noreply.github.com>
2019-04-29 12:18:16 +0300

    Started modal

---
+++ astra.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="styles/modal.css">
+
+    <title>Astra</title>
+</head>
+<body>
+    <h3>0 korrus</h3>
+    <img id="floor0" src="pildid/AstraSilva/Astra-Silva0.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <h3>1 korrus</h3>
+    <img src="pildid/AstraSilva/Astra-Silva1.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <h3>2 korrus</h3>
+    <img src="pildid/AstraSilva/Astra-Silva2.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <h3>3 korrus</h3>
+    <img src="pildid/AstraSilva/Astra-Silva3.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <h3>4 korrus</h3>
+    <img src="pildid/AstraSilva/Astra-Silva4.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <h3>5 korrus</h3>
+    <img src="pildid/AstraSilva/Astra-Silva5.jpg" alt="astra ja silva 0 korrus" style="width:350px;height:350px;">
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+    <!-- The Close Button -->
+    <span class="close">&times;</span>
+    <!-- Modal Content (The Image) -->
+    <img class="modal-content" id="img01">
+
+    <!-- Modal Caption (Image Text) -->
+    <div id="caption"></div>
+  </div>
+</body>
+<script src="modal.js"></script>
+</html>
