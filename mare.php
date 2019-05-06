6a9dafc471d16e09cd4c254960bd1fd864c0da45
stevsal <stevsal@users.noreply.github.com>
2019-05-06 14:50:45 +0300

    mare testimine

---
+++ mare.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="styles/modal.css">
+  <title>Mare</title>
+</head>
+<body>
+  <div class="container">
+      <div class="title">
+        <ul>
+          <li><a class="title-astra/silva" href="index.html">Tagasi pealehele</a></li>
+        </ul>
+      </div>
+      </div>
+      <div class = "pildid">
+      <img class="ASpics" src="pildid/Mare/mare11.png" alt="mare esimene korrus" style="width:300px;height:300px;margin-left: 5px;">
+      <img class="ASpics" src="pildid/Mare/mare2.png" alt="mare teine korrus" style="width:300px;height:300px;margin-left: 5px;">
+      <img class="ASpics" src="pildid/Mare/mare3.png" alt="mare kolmas korrus" style="width:300px;height:300px;margin-left: 5px;">
+      <img class="ASpics" src="pildid/Mare/mare4.png" alt="mare neljas korrus" style="width:300px;height:300px;margin-left: 5px;">
+      <img class="ASpics" src="pildid/Mare/mare5.png" alt="mare viies korrus" style="width:300px;height:300px;margin-left: 5px;">
+      <img class="ASpics" src="pildid/Mare/mare6.png" alt="mare kuues korrus" style="width:300px;height:300px;margin-left: 5px;">
+      </div>
+      <!-- The Modal -->
+      <div id="myModal" class="modal">
+      <!-- The Close Button -->
+      <span class="close" onclick="document,getElementById('myModal').style.display='none'">&times;</span>
+      <!-- Modal Content (The Image) -->
+      <img class="modal-content" id="img01">
+
+      <!-- Modal Caption (Image Text) -->
+      <div id="caption"></div>
+    </div>
+  </body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="modal.js"></script>
+  </html>
